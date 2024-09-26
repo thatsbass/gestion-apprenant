8e58f6e39a810edad4e1bb1b8408a259807fa551
thatsbass <bass374@outlook.com>
2024-09-26 21:56:59 +0000

    routing

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,10 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Routes publiques
-Route::post('login', [AuthController::class, 'login']);
+Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 // Routes protégées
 Route::middleware(['auth:api'])->group(function () {
-    Route::post('logout', [AuthController::class,'logout']);
-    // Route::get('profile', [UserController::class, 'profile']);
-    // Ajoutez d'autres routes protégées ici
+    Route::post('auth/logout', [AuthController::class,'logout']);
 });
-
-// Routes spécifiques pour l'authentification par Passport
-// Route::middleware(['auth:api'])->group(function () {
-//     Route::get('users', [UserController::class, 'index']);
-// });
