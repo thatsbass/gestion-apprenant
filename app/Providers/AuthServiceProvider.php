0987cdbce673c44380129bce51d7049f1e275189
thatsbass <bass374@outlook.com>
2024-10-02 19:05:14 +0200

    release

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,10 +7,8 @@ use App\Factories\AuthFactory;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
-        $this->app->singleton('AuthService', function ($app) {
-            return AuthFactory::make(config('db_service.default'));
-        });
+        $this->app->singleton('AuthService', fn($app) => AuthFactory::make(config('db_service.default')));
     }
 }
